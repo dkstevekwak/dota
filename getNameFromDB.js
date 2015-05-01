4ae3c9df8690bfb814471f3489caaaede3d7d39f
Steve Kwak <steveguac@gmail.com>
2015-05-01 16:11:25 -0400

    edit removed

---
+++ getNameFromDB.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var mainPage = fs.readFileSync('./body.txt').toString();
+
+$ = cheerio.load(mainPage);
+var name = $('#content-header').find('img').attr('alt');
+
+console.log(name);

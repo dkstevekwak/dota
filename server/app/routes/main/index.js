742105c1b61baff0e7bcd8ee73adea64921bc93c
Alex Wang <alwang85@gmail.com>
2015-05-01 16:59:49 -0400

    hopefully have a functional route

--- server/app/routes/main/index.js
+++ server/app/routes/main/index.js
@@ -1,6 +1,9 @@
 'use strict';
 var router = require('express').Router();
 var Heroes = require('mongoose').model('heroStat');
+var PersonalStat = require('mongoose').model('personalStat');
+var async = require('async');
+var cheerio = require('cheerio');
 var _ = require('lodash');
 module.exports = router;
 
@@ -21,3 +24,73 @@ router.put('/:heroId', function(req,res,next){
         })
     })
 });
+router.post('/serverLog', function(req, res, next){
+  var entireString = req.body;
+  var allFriendIDs = getFriendId();
+  var lastTenFriendIDs = allFriendIDs.slice(-10);
+  console.log(lastTenFriendIDs);
+
+  function getFriendId () {
+    var pat = /U:1:(\d*)/g;
+    var arrToReturn = [];
+    var val;
+    while (val = pat.exec(entireString)) {
+      arrToReturn.push(val[1]);
+    }
+    console.log('hoping this runs before serverLog');
+    return arrToReturn;
+  };
+  console.log('here is the hypothetical list of friends ', allFriendIDs );
+
+  async.forEachLimit(allFriendIDs, 1, function(player, done){
+    var tempUrl = 'http://www.dotabuff.com/players/' + player + '/heroes?date=year&skill_bracket=&lobby_type=&game_mode=all_pick&faction=&duration=&enjoyment=any&metric=played';
+    var options = {
+      url: tempUrl,
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36' }
+    };
+    request(options, function (error, response, body) {
+      $ = cheerio.load(body);
+      var array = [];
+      $('tr').each(function(i,elem){
+        array.push([$(this).find("img").attr('alt'), $(this).find("td").first().next().next().text(), $(this).find("td").first().next().next().next().text(), $(this).find("td").first().next().next().next().next().text()]);
+      });
+      array.shift(); //generates all hero stuff
+      var playerName = $('#content-header').find('img').attr('alt');
+      var results = [];
+      var obj = {
+        user: playerName,
+        userId: player,
+        proficiency: []
+      };
+      var tempObj = {};
+      array.forEach(function (el) {
+        tempObj = {};
+        tempObj.name = el[0];
+        tempObj.games = el[1];
+        tempObj.winRate = (el[2].replace('\.', '').replace('\%', '')) / 10000;
+        tempObj.kda = el[3];
+        obj.proficiency.push(tempObj);
+      });
+      PersonalStat.create(obj, function(err, personalData){
+        console.log(personalData);
+        results.push(personalData);
+        done();
+      })
+    });
+  }, function(err){
+    res.send(results);
+  })
+  //for each player
+  //parse player specific data
+  //push to database based on friendID
+  //create a proficiency hash object
+  //pass to front end
+});
+
+
+
+
+
+

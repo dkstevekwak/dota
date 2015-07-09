39d800c64e02e3ea6eba3330c9574ea3a90ed7b9
Alex Wang <alwang85@gmail.com>
2015-07-09 17:52:03 -0400

    took spaces in log into account

--- webkit-client/js/parseNames.js
+++ webkit-client/js/parseNames.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
   var possiblePaths = {
     linux: "~/.steam/root/SteamApps/common/dota 2 beta/dota/server_log.txt",
     darwin: "/Library/Application Support/Steam/steamapps/common/dota 2 beta/dota/server_log.txt",
-    win: "C:&#92;Program Files (x86)&#92;Steam&#92;steamapps&#92;common&#92;dota 2 beta&#92;dota&#92;server_log.txt"
+    win: "C:/Program Files (x86)/Steam/steamapps/common/dota 2 beta/dota/server_log.txt"
   }
 
   var currentOS = os.platform();
@@ -25,19 +25,21 @@ $(document).ready(function () {
 
   //NOTE: fs API documentation states to use fs.watch, but its not working on macs, so using fs.watchFile.
 
-  fs.watchFile(path, function(event, filename){
-      var allLogs = fs.readFileSync(path, {encoding: 'utf8'}, function (err, file) {
-        if (err) throw err;
-      });
-      allLogs = allLogs.split("\n");
-      var log = allLogs[allLogs.length - 1];
-      console.log(log);
-      var params = [
-        'height='+screen.height,
-        'width='+screen.width,
-        'fullscreen=yes' // only works in IE, but here for completeness
-      ].join(',');
-      window.open("https://immense-ridge-1885.herokuapp.com/?logs=" + log, params);
+  fs.watchFile(path, function(curr, prev){
+    var allLogs = fs.readFileSync(path, {encoding: 'utf8'}, function (err, file) {
+      if (err) throw err;
+      console.log('inside fsRead',file);
+    });
+
+    allLogs = allLogs.split("\n");
+    var log = (allLogs[allLogs.length - 1].length > 0 ) ? allLogs[allLogs.length - 1].replace(/=/, "") : allLogs[allLogs.length - 2].replace(/=/, "");
+    console.log("HERE IS THE LOG: ", log);
+    var params = [
+      'height='+screen.height,
+      'width='+screen.width,
+      'fullscreen=yes' // only works in IE, but here for completeness
+    ].join(',');
+    window.open("https://immense-ridge-1885.herokuapp.com/?logs=" + log, params);
   })
   // ---------------------------------------------------------------------
 

db5fc90ccfc67e7e4762313ef50651d039c13e52
Alex Wang <alwang85@gmail.com>
2015-07-10 16:57:43 -0400

    functional version of desired product

--- webkit-client/js/parseNames.js
+++ webkit-client/js/parseNames.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
 
   var fs = require('fs');
   var os = require('os');
+  var async = require('async');
+  var cheerio = require('cheerio');
+  var request = require('request');
   var $input = $('#input');
   var $choose = $('#choose');
   var $output = $('#status');
   var $start = $('#start');
   var path = "";
+  var results = []; //eventual array of 10 player data;
   var log;
   var possiblePaths = {
     linux: "~/.steam/root/SteamApps/common/dota 2 beta/dota/server_log.txt",
@@ -36,13 +40,79 @@ $(document).ready(function () {
 
       allLogs = allLogs.split("\n");
       var log = (allLogs[allLogs.length - 1].length > 0 ) ? allLogs[allLogs.length - 1].replace(/=/, "") : allLogs[allLogs.length - 2].replace(/=/, "");
-      console.log("HERE IS THE LOG: ", log);
-      var params = [
-        'height='+screen.height,
-        'width='+screen.width,
-        'fullscreen=yes' // only works in IE, but here for completeness
-      ].join(',');
-      window.open("https://immense-ridge-1885.herokuapp.com/?logs=" + log, params);
+      //console.log("HERE IS THE LOG: ", log);
+      //var params = [
+      //  'height='+screen.height,
+      //  'width='+screen.width,
+      //  'fullscreen=yes' // only works in IE, but here for completeness
+      //].join(',');
+      //window.open("https://immense-ridge-1885.herokuapp.com/?logs=" + log, params);
+      function getFriendId (entireString) {
+        var pat = /U:1:(\d*)/g;
+        var arrToReturn = [];
+        var val;
+        while (val = pat.exec(entireString)) {
+          arrToReturn.push(val[1]);
+        }
+        console.log('hoping this runs before serverLog', arrToReturn);
+        return arrToReturn;
+      };
+
+      var allFriendIDs = getFriendId(log);
+      var lastTenFriendIDs = allFriendIDs.slice(0, 10);
+      var results = [];
+      async.forEachLimit(lastTenFriendIDs, 1, function(player, done){
+        console.log('inside async now');
+        var tempUrl = 'http://www.dotabuff.com/players/' + player + '/heroes?date=year&skill_bracket=&lobby_type=&game_mode=all_pick&faction=&duration=&enjoyment=any&metric=played';
+        var options = {
+          url: tempUrl,
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36' }
+        };
+        request(options, function (error, response, body) {
+          console.log(body);
+          var $ = cheerio.load(body);
+          var array = [];
+          $('tr').each(function(i,elem){
+            array.push([$(this).find("img").attr('alt'), $(this).find("td").first().next().next().text(), $(this).find("td").first().next().next().next().text(), $(this).find("td").first().next().next().next().next().text()]);
+          });
+          array.shift(); //generates all hero stuff
+          var playerName = $('#content-header').find('img').attr('alt');
+          var playerInfo = [$('#content-header-secondary').find('dd').find('time').text(),$('#content-header-secondary').find('.wins').text(),$('#content-header-secondary').find('.losses').text(),$('#content-header-secondary').find('.abandons').text()];
+
+          var obj = {
+            user: playerName,
+            userId: player,
+            userInfo: playerInfo,
+            proficiency: [],
+            selectedHero: null
+          };
+          var tempObj = {};
+          array.forEach(function (el) {
+            tempObj = {};
+            tempObj.name = el[0];
+            tempObj.games = el[1];
+            tempObj.winRate = (el[2].replace('\.', '').replace('\%', '')) / 10000;
+            tempObj.kda = el[3];
+            obj.proficiency.push(tempObj);
+          });
+          //PersonalStat.create(obj, function(err, personalData){
+          //  if(err) console.log(err);
+          //  console.log('personal data created', personalData);
+          results.push(obj);
+          done();
+          //})
+          console.log('here are some results', results);
+        });
+      }, function(err){
+        if (err) console.log(err);
+        var stringified = JSON.stringify(results);
+        console.log('we are inside the error handler now');
+        window.open("http://localhost:1337/?logs=" + log + "&playerLogs=" + stringified);
+      });
+
+
     })
   }
 
